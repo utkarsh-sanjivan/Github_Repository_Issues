15fc32814215098aa27ad883c60e23bf31872c06
Utkarsh Sanjivan <utkarsh.sannu@gmail.com>
2020-07-20 22:56:47 +0530

    Routing Fix

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter as Router, HashRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import IssueHeader from './component/Header/index';
 import IssueSearch from './container/IssueSearch/index';
 import IssueList from './container/IssueList/index';
