d4752b2685334002620414bdaf343cb59ea49667
Utkarsh Sanjivan <utkarsh.sannu@gmail.com>
2020-07-21 00:11:07 +0530

    Added No Details Image

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -4,8 +4,8 @@ import './style.css';
 const IssueHeader = props => {
   return (
     <div className="issues-page-header-wrapper">
-      <div className="issues-page-header-title">{window.location.pathname === '/'? 'Issue  Search': window.location.pathname.includes('issue-details')? 'Issue Details':'Issue List'}</div>
-      <div className="issues-page-header-sub-title">{window.location.pathname.includes('issue-details')? null: 'Search issues from a public repository of a user'}</div>
+      <div className="issues-page-header-title">{window.location.pathname === '/'? 'Issue  Search': window.location.pathname.includes('issue-list')? 'Issue List':'Issue Details'}</div>
+      <div className="issues-page-header-sub-title">{window.location.pathname.includes('issue-list')? 'Search issues from a public repository of a user': null}</div>
     </div>
   );
 }
--- src/container/IssueDetails/index.js
+++ src/container/IssueDetails/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import MarkdownGithub from 'react-markdown-github';
-import { Skeleton, Avatar } from 'antd';
+import { Skeleton, Avatar, Empty } from 'antd';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import * as IssueListActions from './../../store/IssueList/actions';
 import StatusTag from './../../component/StatusTag/index';
@@ -16,6 +16,7 @@ class IssueDetails extends React.PureComponent {
     this.state = {
         currentIssue: this.props.issueList.issueList.find(issue => issue.number.toString() === this.props.match.params.issueId),
         commentsList: [],
+        issueNotFound: false,
     };
   }
 
@@ -29,13 +30,18 @@ class IssueDetails extends React.PureComponent {
 
     componentDidUpdate(prevProps) {
         if ( JSON.stringify(prevProps.issueList.commentList) !== JSON.stringify(this.props.issueList.commentList) ) {
-           this.setState({ commentList: this.props.issueList.commentList });
+            this.setState({ commentList: this.props.issueList.commentList });
         }
         if (
             prevProps.issueList.issue.id !== this.props.issueList.issue.id &&
             prevProps.issueList.issue.number !== this.props.issueList.issue.number
         ) {
-           this.setState({ currentIssue: this.props.issueList.issue });
+            this.setState({ currentIssue: this.props.issueList.issue });
+        }
+        if (
+            !prevProps.issueList.issueError && this.props.issueList.issueError
+        ) {
+            this.setState({ issueNotFound: true })
         }
     }
   
@@ -47,65 +53,70 @@ class IssueDetails extends React.PureComponent {
                 loading={true}
                 active
             />
-            : <div className="issue-details-wrapper">
-                <div className="issue-details-title">{this.state.currentIssue && this.state.currentIssue.title}</div>
-                <div className="issue-details-sub-title">
-                    <StatusTag
-                        color={this.state.currentIssue && this.state.currentIssue.state === 'open'? '#28a745': '#f50'}
-                        icon={this.state.currentIssue && this.state.currentIssue.state === 'open'? 
-                            <CheckOutlined twoToneColor='#28a745' />
-                            : <CloseOutlined twoToneColor='#f50' />
-                        }
-                        text={this.state.currentIssue && this.state.currentIssue.state === 'open'? 'Open': 'Close'}
-                    />
-                    <span><b>{this.state.currentIssue && this.state.currentIssue.user.login}</b> openened this issue {this.state.currentIssue && formatDateString(this.state.currentIssue.created_at)}, {this.props.issueList.commentList.length} comments</span>
-                </div>
-                <div className="issue-details-description-container">
-                    <div className="issue-desc-header">
-                        <Avatar src={this.state.currentIssue && this.state.currentIssue.user.avatar_url} className='issue-details-raisedby-avatar'/>
-                        <span className="issue-details-raisedby-description"><b>{this.state.currentIssue && this.state.currentIssue.user.login}</b> {this.state.currentIssue && ` posted ${formatDateString(this.state.currentIssue.created_at)}`}</span>
-                    </div>
-                    <MarkdownGithub 
-                        source={this.state.currentIssue && this.state.currentIssue.body}
-                        className='issue-details-description-content'
-                    />
-                </div>
-                <div className="tags-container">
-                    {this.state.currentIssue && this.state.currentIssue.labels.map(value => (
-                        <span 
-                            style={{ 
-                                backgroundColor: `#${value.color}`,
-                                color: `${value.color === '000'? '#fff': '#000'}`,
-                                border: `${value.color === 'ffffff'? '#9ca2a8 1px solid': 'none'}`,
-                            }}
-                            className="issue-tag-card"
-                        >
-                            {value.name}
-                        </span>
-                        ))}
-                </div>
-                <div className="issue-details-comment-container">
-                    { this.props.issueList.commentListLoading?
-                        <Skeleton 
-                            className="comments-skeleton"
-                            loading={true}
-                            active
+            : this.state.issueNotFound?
+                <Empty 
+                    className="table-empty-image"
+                    description={'Issues Unavailable'}
+                />
+                : <div className="issue-details-wrapper">
+                    <div className="issue-details-title">{this.state.currentIssue && this.state.currentIssue.title}</div>
+                    <div className="issue-details-sub-title">
+                        <StatusTag
+                            color={this.state.currentIssue && this.state.currentIssue.state === 'open'? '#28a745': '#f50'}
+                            icon={this.state.currentIssue && this.state.currentIssue.state === 'open'? 
+                                <CheckOutlined twoToneColor='#28a745' />
+                                : <CloseOutlined twoToneColor='#f50' />
+                            }
+                            text={this.state.currentIssue && this.state.currentIssue.state === 'open'? 'Open': 'Close'}
                         />
-                        : this.state.commentList && this.state.commentList.map(comment => (
-                            <div className="issue-details-description-container">
-                                <div className="issue-desc-header">
-                                    <Avatar src={comment.user.avatar_url} className='issue-details-raisedby-avatar'/>
-                                    <span className="issue-details-raisedby-description"><b>{comment.user.login}</b> {this.state.currentIssue && ` commented ${formatDateString(comment.created_at)}`}</span>
+                        <span><b>{this.state.currentIssue && this.state.currentIssue.user.login}</b> openened this issue {this.state.currentIssue && formatDateString(this.state.currentIssue.created_at)}, {this.props.issueList.commentList.length} comments</span>
+                    </div>
+                    <div className="issue-details-description-container">
+                        <div className="issue-desc-header">
+                            <Avatar src={this.state.currentIssue && this.state.currentIssue.user.avatar_url} className='issue-details-raisedby-avatar'/>
+                            <span className="issue-details-raisedby-description"><b>{this.state.currentIssue && this.state.currentIssue.user.login}</b> {this.state.currentIssue && ` posted ${formatDateString(this.state.currentIssue.created_at)}`}</span>
+                        </div>
+                        <MarkdownGithub 
+                            source={this.state.currentIssue && this.state.currentIssue.body}
+                            className='issue-details-description-content'
+                        />
+                    </div>
+                    <div className="tags-container">
+                        {this.state.currentIssue && this.state.currentIssue.labels.map(value => (
+                            <span 
+                                style={{ 
+                                    backgroundColor: `#${value.color}`,
+                                    color: `${value.color === '000'? '#fff': '#000'}`,
+                                    border: `${value.color === 'ffffff'? '#9ca2a8 1px solid': 'none'}`,
+                                }}
+                                className="issue-tag-card"
+                            >
+                                {value.name}
+                            </span>
+                            ))}
+                    </div>
+                    <div className="issue-details-comment-container">
+                        { this.props.issueList.commentListLoading?
+                            <Skeleton 
+                                className="comments-skeleton"
+                                loading={true}
+                                active
+                            />
+                            : this.state.commentList && this.state.commentList.map(comment => (
+                                <div className="issue-details-description-container">
+                                    <div className="issue-desc-header">
+                                        <Avatar src={comment.user.avatar_url} className='issue-details-raisedby-avatar'/>
+                                        <span className="issue-details-raisedby-description"><b>{comment.user.login}</b> {this.state.currentIssue && ` commented ${formatDateString(comment.created_at)}`}</span>
+                                    </div>
+                                    <MarkdownGithub 
+                                        source={comment.body}
+                                        className='issue-details-description-content'
+                                    />
                                 </div>
-                                <MarkdownGithub 
-                                    source={comment.body}
-                                    className='issue-details-description-content'
-                                />
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
                 </div>
-            </div>
         );
     }
 
